3a890f9f5f1cd6c252d9421a6ef36d2f5fbbecd1
unknown <rvechas@calance.com>
2022-09-01 12:52:47 +0530

    #User profile Added #Update User Profile added, need to add more fields in user profile #link Added to user picture in header

--- webclient/src/App.js
+++ webclient/src/App.js
@@ -11,6 +11,9 @@ import AppUserHome from "./Pages/AppUserHome/AppUserHome";
 import SingleQuizDetail from "./Components/SingleQuizDetail/SingleQuizDetail";
 import AppliedContestList from "./Components/AppliedContestList/AppliedContestList";
 import SingleQuestion from "./Components/SingleQuestion/SingleQuestion";
+import UserProfile from "./Pages/UserProfile/UserProfile";
+import UpdateProfile from "./Pages/UserProfile/UpdateProfile";
+
 
 
 function App() {
@@ -28,6 +31,11 @@ function App() {
         <Route exact path="/singlequizdetail" element={<SingleQuizDetail />} />
         <Route exact path="/appliedcontest" element={<AppliedContestList />} />
         <Route exact path="/singlequestion" element={<SingleQuestion />} />
+        <Route exact path="/userprofile" element={<UserProfile />} />
+        <Route exact path="/updateprofile" element={<UpdateProfile />} />
+
+        
+        
         
         
         
---
+++ webclient/src/Pages/UserProfile/UserProfile.js
@@ -0,0 +1,126 @@
+import React, { useEffect, useState } from 'react';
+//API URL
+import CONSTANTS from '../../Constants/global'
+import { BrowserRouter as Router, useNavigate, Link } from "react-router-dom";
+
+//import common components
+import AppHeader from "../../Components/AppHeader/AppHeader";
+import BottomNav from "../../Components/BottomNav/BottomNav";
+import axios from 'axios';
+
+import { ToastContainer, toast } from 'react-toastify';
+
+export default function UserProfile() {
+    const navigate = useNavigate();
+    const [userInfo, setUserInfo] = useState();
+
+    const getUserDetails = async () => {
+        const _URL = CONSTANTS.GETUSERPROFILE;
+        const token = sessionStorage.getItem("token");
+        const HEADERS = { "authorization": token, }
+        try {
+            axios.get(_URL, {
+                headers: HEADERS,
+            }).then((res) => {
+                console.log(res);
+                setUserInfo(res?.data);
+            })
+
+        } catch (error) {
+            console.log(error.response)
+            if (error && error.response) {
+                console.log('error is ', error)
+                toast.error(error.response.data.message);
+                if (error.response.status === 401) {
+                    navigate("/");
+                }
+            } else {
+                toast.error('Something went wrong');
+            }
+        }
+    }
+    useEffect(() => {
+        getUserDetails();
+    }, [])
+    return (
+        <>
+            <AppHeader></AppHeader>
+            <div className="page-container">
+                <section className='profile-banner'>
+                    <img className='img-fluid w-100' src="./assets/images/banner.webp" alt="Profile Banner" />
+                    <div className="banner-text">
+                        <div className="user-detail text-center">
+                            <h2 className='text-white mb-0 text-shadow-gray'>{userInfo?.name}</h2>
+                            <h5 className='text-white mt-0 text-shadow-gray'>{userInfo?.location}</h5>
+                        </div>
+                    </div>
+                </section>
+
+                <div className="container-fluid ">
+                    {/* Need to update stats later */}
+                    {/* <div className="row">
+                    <section className="profile-stats">                       
+                            <div className="col-sm-12">
+                                <div className="stats-box">
+                                    <ul className="list-group">
+                                        <li className="list-group-item">An item</li>
+                                        <li className="list-group-item">A second item</li>
+                                        <li className="list-group-item">A third item</li>
+                                        <li className="list-group-item">A fourth item</li>
+                                        <li className="list-group-item">And a fifth one</li>
+                                    </ul>
+                                </div>
+                            </div>                        
+                    </section>
+                </div> */}
+                    <div className="row">
+                        <Link to={'/updateprofile'} state={{ userData: userInfo }}>
+                            <div className="col-sm-12 my-5">
+                                <div className="user-box d-flex justify-content-start">
+                                    <div className="col-1 pt-4"><i className="fas fa-user-alt"></i></div>
+                                    <div className='col-9'>
+                                        <h3 className='mb-0'>My Account</h3>
+                                        <p>Edit your information</p>
+                                    </div>
+                                    <div className="col-2 pt-4"><i className="fas fa-arrow-right"></i></div>
+                                </div>
+                            </div>
+                        </Link>
+                    </div>
+                    <div className="row">
+                        <h2 className='fs-6 fw-bold text-danger'>This will be replaced with add list once merged with Ads</h2>
+                        <h2 className='fs-4 text-uppercase'>Answer Questions To win Money</h2>
+                        <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+                            <div className="carousel-inner">
+                                <div className="carousel-item active">
+                                    <img src="./assets/images/banner.webp" className="d-block w-100" alt="..." />
+                                </div>
+                                <div className="carousel-item">
+                                    <img src="./assets/images/ques-img.jpeg" className="d-block w-100" alt="..." />
+                                </div>
+                                <div className="carousel-item">
+                                    <img src="./assets/images/banner.webp" className="d-block w-100" alt="..." />
+                                </div>
+                                <div className="carousel-item">
+                                    <img src="./assets/images/ques-img.jpeg" className="d-block w-100" alt="..." />
+                                </div>
+                                <div className="carousel-item">
+                                    <img src="./assets/images/ques-img.jpeg" className="d-block w-100" alt="..." />
+                                </div>
+                            </div>
+                            {/* <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span className="sr-only">Previous</span>
+                            </a>
+                            <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span className="sr-only">Next</span>
+                            </a> */}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <BottomNav />
+        </>
+    )
+}
